5103b617f7a82e35bed2bfdc379d7abdbea1bfe7
Jean-Louis Boudart <jeanlouis@confluent.io>
2022-07-01 18:52:05 +0200

    documenting the endpoints

--- src/main/java/com/example/demo/controllers/MyEndpointResource.java
+++ src/main/java/com/example/demo/controllers/MyEndpointResource.java
@@ -3,6 +3,7 @@ package com.example.demo.controllers;
 import com.example.demo.models.PaymentDTO;
 import com.example.demo.services.PaymentPublisher;
 import com.example.demo.services.PaymentReceiver;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -19,25 +20,28 @@ public class MyEndpointResource {
     private final PaymentReceiver paymentReceiver;
     private final KafkaProducer<byte[], byte[]> poisonPillProducer;
 
+    @GetMapping("/payments")
+    @Operation(summary = "Consume payments")
+    public List<PaymentDTO> getPayments() {
+        return paymentReceiver.read();
+    }
     @PostMapping("/payment")
+    @Operation(summary = "Produce a valid payment in Kafka")
     public ResponseEntity<?> createPayment(@RequestBody PaymentDTO paymentDTO) {
         paymentPublisher.publish(paymentDTO);
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/payments")
-    public List<PaymentDTO> getPayments() {
-        return paymentReceiver.read();
-    }
-
-    @GetMapping("/poison-pill")
-    public ResponseEntity<?> createPoisonPill(@RequestParam("message") String message) {
+    @PostMapping("/poison-pill")
+    @Operation(summary = "Produce a poison pill (invalid record) in Kafka, consuming after this might trigger errors")
+    public ResponseEntity<?> createPoisonPill(@RequestBody String message) {
         poisonPillProducer.send(new ProducerRecord<>(PaymentPublisher.TOPIC_NAME, message.getBytes()));
         return ResponseEntity.badRequest().build();
     }
 
-    @GetMapping("/tombstone")
-    public ResponseEntity<?> createTombstone(@RequestParam("key") String key) {
+    @PostMapping("/tombstone")
+    @Operation(summary = "Produce a valid message in Kafka, but this message contains a case (a tombstone) that is not handled by the consumer (processing error)")
+    public ResponseEntity<?> createTombstone(@RequestBody String key) {
         poisonPillProducer.send(new ProducerRecord<>(PaymentPublisher.TOPIC_NAME, key.getBytes(), null));
         return ResponseEntity.badRequest().build();
     }
