7d9406847ea48b5df07999a8a576dae4a65992b6
jeanlouisboudart <jeanlouis.boudart@gmail.com>
2022-07-05 09:25:47 +0200

    rethink offset commit OOTB when enable.auto.commit is enabled, kafka client will automatically commit offsets on shutdown.

--- src/main/java/com/example/demo/services/PaymentReceiver.java
+++ src/main/java/com/example/demo/services/PaymentReceiver.java
@@ -52,12 +52,4 @@ public class PaymentReceiver {
         }
 
     }
-
-    @PreDestroy
-    public void onShutdown() {
-        logger.info("Committing Kafka Messages before shutdown");
-        consumer.commitSync();
-        //consumer.close() not required here since Consumer interface extends Closeable, Spring will automatically close it.
-    }
-
 }
